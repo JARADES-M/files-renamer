7689ab0daad3eb2b7b5c258e0d084347cbc60e82
Jarades <jaradesm@gmail.com>
2024-08-03 19:36:23 -0300

    Refactoring

--- FilesRenamer.java
+++ FilesRenamer.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class FilesRenamer {
     public static void main(String[] args) {
@@ -16,25 +17,24 @@ public class FilesRenamer {
         String namePattern = args[0];
         String replacePattern = args[1];
         String replaceValue = args[2];
-                
         File dir = new File(".");
         File[] filesList = dir.listFiles((d, n) -> n.matches(namePattern));
-        
-        if (filesList != null) {
-            for (File file : filesList) {
-                String originalName = file.getName();
-                
-                String newName = originalName.replaceAll(replacePattern, replaceValue);
-                
-                File newFile = new File(dir, newName);
-                if (file.renameTo(newFile)) {
-                    System.out.println("Renamed: " + originalName + " -> " + newName);
-                } else {
-                    System.out.println("Failed to rename: " + originalName);
-                }
-            }
-        } else {
+
+        if (filesList == null || filesList.length == 0) {
             System.out.println("Directory is invalid or contains no matching files.");
+            return;
+        }
+
+        for (File file : filesList) {
+            String originalName = file.getName();
+            String newName = originalName.replaceAll(replacePattern, replaceValue);
+            File newFile = new File(dir, newName);
+
+            if (file.renameTo(newFile)) {
+                System.out.println("Renamed: " + originalName + " -> " + newName);
+            } else {
+                System.out.println("Failed to rename: " + originalName);
+            }
         }
     }
 }
